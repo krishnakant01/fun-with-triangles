3981c2acdb0ff076e6badfad347bb1593999d8b8
krishnakant01 <63720159+krishnakant01@users.noreply.github.com>
2021-09-06 00:47:59 +0530

    //beautified

--- area.js
+++ area.js
@@ -3,9 +3,9 @@ const height = document.querySelector("#height");
 const buttonArea = document.querySelector("#btn-calc-area");
 const outputDivArea = document.querySelector("#output-div-area");
 
-function calculateArea(){
-var area = 0.5*Number(base.value)*Number(height.value);
-outputDivArea.innerText = "Area = "+area;
+function calculateArea() {
+    var area = 0.5 * Number(base.value) * Number(height.value);
+    outputDivArea.innerText = "Area = " + area;
 }
 
-buttonArea.addEventListener("click",calculateArea);
+buttonArea.addEventListener("click", calculateArea);
--- hypotenuse.js
+++ hypotenuse.js
@@ -3,10 +3,12 @@ const b = document.querySelector("#b");
 const buttonHypotenuse = document.querySelector("#btn-calc-hypotenuse");
 const outputDivHypotenuse = document.querySelector("#output-div-hypo");
 
-function calculateArea(){
-    var x = (Number(a.value)*Number(a.value)+Number(b.value)*Number(b.value));
-var hypotenuse = Math.sqrt(x).toPrecision(3);
-outputDivHypotenuse.innerText = "Hypotenuse = "+hypotenuse;
+function calculateArea() {
+    var x = Math.pow(a.value, 2) + Math.pow(b.value, 2);
+    //var x = (Number(a.value)*Number(a.value)+Number(b.value)*Number(b.value));
+    var hypotenuse = Math.sqrt(x).toPrecision(3);
+    outputDivHypotenuse.innerText = "Hypotenuse = " + hypotenuse;
+
 }
 
-buttonHypotenuse.addEventListener("click",calculateArea);
+buttonHypotenuse.addEventListener("click", calculateArea);
